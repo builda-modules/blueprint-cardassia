17ae4246cf8e4a3e44a623370c3ba3d3453b0c28
Alex Foxleigh <alex@alexfoxleigh.com>
2022-10-31 19:13:15 +0000

    Initial commit of blueprint assets

---
+++ README.md
@@ -0,0 +1,5 @@
+# Builda Blueprint: Cardassia
+
+Cardassia is a super-basic react typescript blueprint which can be used to generate a new component. I uses SCSS modules for styling.
+
+It outputs an index.tsx file and a styles.module.scss file.
---
+++ files/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+/**
+ * The `%PASCAL_CASE%` component
+ * @returns {JSX.Element}
+ */
+export const %PASCAL_CASE%: JSX.Element = () => {
+
+
+  return (
+    
+        <div className={styles['%KEBAB_CASE%']}>
+          Your content goes here.
+        </div>
+    
+  );
+};
+
+%PASCAL_CASE%.displayName = '%PASCAL_CASE%';
+
+export default %PASCAL_CASE%;
